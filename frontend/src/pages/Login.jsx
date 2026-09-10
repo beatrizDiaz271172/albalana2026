518443d98f8633ea8ebe09aa2fb05bacacd32dc9
bdiaz2711 <di.beatriz@gmail.com>
2026-09-10 20:14:27 -0300

    RAILWAY

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setCargando(true);
 
     try {
-      const response = await fetch(API_URL+'/auth/login', {
+      const response = await fetch(API_URL+'/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
