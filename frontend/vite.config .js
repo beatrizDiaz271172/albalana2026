0e7041546b5043bd020b4b183378c4fe77bcc324
bdiaz2711 <di.beatriz@gmail.com>
2026-09-18 11:08:10 -0300

    Fix nginx configuration

--- frontend/vite.config .js
+++ frontend/vite.config .js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8081,
-    host: true
-  },
-  preview: {
-    port: 8081,
-    host: true
+    host: '0.0.0.0',  // Importante para Docker
+    port: 5173
   }
 })
