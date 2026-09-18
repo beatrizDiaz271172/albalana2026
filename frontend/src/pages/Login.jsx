163b340c78ed654a539bc2d2511db186a9af979d
bdiaz2711 <di.beatriz@gmail.com>
2026-09-18 16:40:46 -0300

    path en jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.css';
 debugger;
-const API_BASE = import.meta.env.VITE_API_URL;
+/*const API_BASE = import.meta.env.VITE_API_URL;*/
+const API_BASE = "https://albalana2026-production.up.railway.app/api";
 
 const Login = () => {
   const [usuario, setUsuario] = useState('');
