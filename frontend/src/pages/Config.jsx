38d16a3bd78b84fcba1836c0880aa5dd41c74e29
bdiaz2711 <di.beatriz@gmail.com>
2026-09-22 11:14:36 -0300

    Config.jsx

---
+++ frontend/src/pages/Config.jsx
@@ -0,0 +1,2 @@
+const API_BASE = "https://albalana2026-production.up.railway.app/api";
+export default Config;
--- frontend/src/pages/RegistrarIngreso.jsx
+++ frontend/src/pages/RegistrarIngreso.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './RegistrarIngreso.css';
+import './Config.css';
 
-/*const API_BASE = import.meta.env.VITE_API_URL;*/
-const API_BASE = "https://albalana2026-production.up.railway.app/api";
+/*const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = "https://albalana2026-production.up.railway.app/api";*/
 
 const RegistrarIngreso = () => {
 const navigate = useNavigate();
